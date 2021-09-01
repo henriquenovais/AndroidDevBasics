83f6de2055b5220a7e7011b920eb12982538bdc0
Henrique Novais <henriquennoliveira@gmail.com>
2021-08-31 22:05:09 -0300

    first exercise unit 2

--- RawKotlin/basicSyntax.kt
+++ RawKotlin/basicSyntax.kt
@@ -1,9 +1,9 @@
 class CatholicChurch (var laymen: Int, var clergy: Int, var purgatorySouls: Int) {
     var militantChurch: Int = laymen + clergy
     var sufferingChurch: Int = purgatorySouls
-    
-    fun spreadTheGospel () : Int {
-        Random.nextInt()
+
+    fun spreadTheGospel () : Boolean {
+        return true
     }
 }
 
---
+++ Unit2/ClassAndInheritance/ClassAndInheritance.kt
@@ -0,0 +1,104 @@
+/**
+ * ```
+ *           View
+ *             \/
+ *      TextView
+ *      /            \
+ * ```
+ * EditText Button
+ *
+ * All kotlin classes have a common parent: kotlin.Any
+ */
+
+import kotlin.math.PI
+import kotlin.math.pow
+
+fun main() {
+    /**
+     * An abstract class cannot be instanced. The following code is not possible: val dwelling =
+     * Dwelling()
+     */
+    // val spongeBobSquareCabin = SquareCabin(6)
+    // val roundHut = RoundHut(4)
+    // val roundTower = RoundTower(5)
+
+    val spongeBobSquareCabin = SquareCabin(6, 50.0) 
+    val roundHut = RoundHut(4, 65.0)
+    val roundTower = RoundTower(5, 70.0)
+
+    //  println("\n SquareCabin\n==========")
+    //  println("Capacity: ${spongeBobSquareCabin.capacity}")
+    //  println("Material: ${spongeBobSquareCabin.buildingMaterial}")
+    //  println("Has room? ${spongeBobSquareCabin.hasRoom()}")
+
+    // OR
+
+    with(spongeBobSquareCabin) {
+        println("\n SquareCabin\n==========")
+        println("Capacity: ${capacity}")
+        println("Material: ${buildingMaterial}")
+        println("Has room? ${hasRoom()}")
+        println("Floor area: ${floorArea()}")
+    }
+
+    with(roundHut) {
+        println("\n RoundHut\n==========")
+        println("Capacity: ${capacity}")
+        println("Material: ${buildingMaterial}")
+        println("Has room? ${hasRoom()}")
+        println("Floor area: ${floorArea()}")
+    }
+
+    with(roundTower) {
+        println("\n RoundTower\n==========")
+        println("Capacity: ${capacity}")
+        println("Material: ${buildingMaterial}")
+        println("Has room? ${hasRoom()}")
+        println("Floor area: ${floorArea()}")
+    }
+}
+
+abstract class Dwelling(private var residents: Int) {
+
+    abstract val buildingMaterial: String
+    abstract val capacity: Int
+
+    fun hasRoom(): Boolean {
+        return residents < capacity
+    }
+
+    abstract fun floorArea() : Double
+
+}
+
+// The following syntax is possible in case of needing to declare a constant
+// class ClassName : ParentClass(CONSTANT_VALUE) {}
+
+class SquareCabin(residents: Int, val length : Double) : Dwelling(residents) {
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+
+    override fun floorArea() : Double {
+        return length.pow(2)
+    }
+}
+
+open class RoundHut(residents: Int, val radius : Double) : Dwelling(residents) {
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
+
+    override fun floorArea() : Double {
+        return PI * radius.pow(2)
+    }
+}
+
+class RoundTower(residents: Int, radius : Double, val floors : Int = 2) : RoundHut(residents, radius) {
+    override val buildingMaterial = "Stone"
+    override val capacity = 4 * floors
+
+    override fun floorArea() : Double {
+        return super.floorArea() * floors
+    }
+}
+
+
