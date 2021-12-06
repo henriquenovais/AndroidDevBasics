18a73c57e6514d13b2f855ab1cd74d0592a26271
Henrique Novais <henriquennoliveira@gmail.com>
2021-12-05 22:13:07 -0300

    exercise 3 from unit testing course

--- Testing/unit-testing-in-android-course/unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ Testing/unit-testing-in-android-course/unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,44 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class IntervalsAdjacencyDetectorTest {
 
+    IntervalsAdjacencyDetector SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new IntervalsAdjacencyDetector();
+    }
+
+    @Test
+    public void SUT_isNotNull_true() {
+        Assert.assertNotNull(SUT);
+    }
+
+    @Test
+    public void adjacentInterval() {
+        Interval interval1 = new Interval(3,5);
+        Interval interval2 = new Interval(5,9);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertTrue(var);
+    }
+
+    @Test
+    public void notAdjacentInterval() {
+        Interval interval1 = new Interval(1,2);
+        Interval interval2 = new Interval(5,10);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertFalse(var);
+    }
 }
