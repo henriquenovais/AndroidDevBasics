b6b84a40d6b64fdc835c7be4b5d7cbe146672117
Henrique Novais <henriquennoliveira@gmail.com>
2021-12-07 21:17:47 -0300

    troubleshooting exercise 3 code

--- Testing/unit-testing-in-android-course/unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ Testing/unit-testing-in-android-course/unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -21,9 +21,9 @@ public class IntervalsAdjacencyDetectorTest {
     }
 
     @Test
-    public void isAdjacent_AdjacentIntervalsNoOverlap_true() {
-        Interval interval1 = new Interval(3,5);
-        Interval interval2 = new Interval(5,9);
+    public void isAdjacent_differentSignalAdjacentIntervals_true() {
+        Interval interval1 = new Interval(-5,-3);
+        Interval interval2 = new Interval(-5,9);
         //Arrange
         boolean var = SUT.isAdjacent(interval1, interval2);
         //Act
@@ -32,8 +32,52 @@ public class IntervalsAdjacencyDetectorTest {
     }
 
     @Test
-    public void isAdjacent_notAdjacentIntervalsNoOverlap_false() {
-        Interval interval1 = new Interval(1,2);
+    public void isAdjacent_differentSignalNotAdjacentIntervals_false() {
+        Interval interval1 = new Interval(-5,-2);
+        Interval interval2 = new Interval(6,9);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertTrue(var);
+    }
+
+    @Test
+    public void isAdjacent_differentSignalContainedIntervals_false() {
+        Interval interval1 = new Interval(-5,10);
+        Interval interval2 = new Interval(-2,5);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertFalse(var);
+    }
+
+    @Test
+    public void isAdjacent_positiveSignalAdjacentIntervals_true() {
+        Interval interval1 = new Interval(5,15);
+        Interval interval2 = new Interval(15,30);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertTrue(var);
+    }
+
+    @Test
+    public void isAdjacent_positiveSignalNotAdjacentIntervals_false() {
+        Interval interval1 = new Interval(0,5);
+        Interval interval2 = new Interval(10,15);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertTrue(var);
+    }
+
+    @Test
+    public void isAdjacent_positiveSignalContainedIntervals_false() {
+        Interval interval1 = new Interval(0,25);
         Interval interval2 = new Interval(5,10);
         //Arrange
         boolean var = SUT.isAdjacent(interval1, interval2);
@@ -43,9 +87,31 @@ public class IntervalsAdjacencyDetectorTest {
     }
 
     @Test
-    public void isAdjacent_AdjacentIntervalsWithOverlap_false() {
-        Interval interval1 = new Interval(3,5);
-        Interval interval2 = new Interval(3,9);
+    public void isAdjacent_negativeSignalAdjacentIntervals_true() {
+        Interval interval1 = new Interval(-60,-3);
+        Interval interval2 = new Interval(-3,-1);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertTrue(var);
+    }
+
+    @Test
+    public void isAdjacent_negativeSignalNotAdjacentIntervals_false() {
+        Interval interval1 = new Interval(-75,-59);
+        Interval interval2 = new Interval(-40,-30);
+        //Arrange
+        boolean var = SUT.isAdjacent(interval1, interval2);
+        //Act
+        //Assert
+        Assert.assertTrue(var);
+    }
+
+    @Test
+    public void isAdjacent_negativeSignalContainedIntervals_false() {
+        Interval interval1 = new Interval(-150,-98);
+        Interval interval2 = new Interval(-120,-110);
         //Arrange
         boolean var = SUT.isAdjacent(interval1, interval2);
         //Act
