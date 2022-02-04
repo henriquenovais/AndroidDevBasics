1d226e5f7e7031a9a1e9db2ab630488451a81fcf
Henrique Novais <henriquennoliveira@gmail.com>
2022-02-03 21:19:21 -0300

    finish unit 2 - part 2

--- Unit2/TipCalculator/app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ Unit2/TipCalculator/app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,24 +1,40 @@
 package com.example.tiptime
 
+import androidx.core.content.MimeTypeFilter.matches
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.containsString
+import androidx.test.espresso.assertion.ViewAssertions.matches
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java) //tells to launch specific activity
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tiptime", appContext.packageName)
+    fun calculate_20_percent_tip() {
+    }
+
+    @Test
+    fun calculate_default_tip() {
+        //Arrange
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00"))
+        //Act
+        onView(withId(R.id.calculate_button)).perform(click())
+        //Assert
+        onView(withId(R.id.tip_amount)).check(matches(withText(containsString("$10.00"))))
     }
 }
