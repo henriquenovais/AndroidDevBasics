01db17b2838bd162610424722685355f1f0ba2b3
Henrique Novais <henriquennoliveira@gmail.com>
2022-02-11 14:10:57 -0300

    affirmations app

--- Unit2/Affirmations/app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ Unit2/Affirmations/app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -3,5 +3,7 @@ package com.example.affirmations.data
 import com.example.affirmations.model.Affirmation
 
 class Datasource {
-    fun loadAffirmations() : List<Affirmation>{}
+    fun loadAffirmations() {
+        return listOf<Affirmation>()
+    }
 }
