665e22251457eeebf6c976cc63e5fbbe73f71c5f
Henrique Novais <henriquennoliveira@gmail.com>
2022-02-14 21:04:28 -0300

    implementation of item adapter for Affirmations app

--- Unit2/Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Unit2/Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -1,8 +1,28 @@
 package com.example.affirmations.adapter
 
 import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    //RecyclerView does not interact directly with Views but with ViewHolders
+    class ItemViewHolder( private val view : View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
