0a0c689e9f2c19b07fc6463f709c703839b508b0
Henrique Novais <henriquennoliveira@gmail.com>
2022-05-18 21:49:03 -0300

    Collections exercise

--- RawKotlin/Collections.kt
+++ RawKotlin/Collections.kt
@@ -1,5 +1,10 @@
 fun main() {
     val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
     println("list:   ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+
+    //Set use only unique 
+    val setOfNumbers = numbers.toSet()
+    println("set: ${setOfNumbers}")
 }
 
